f5c455e6c3771eb9b990478d7c7fec0d5d766e37
football in india <footballindia.insta@gmail.com>
2021-06-18 07:49:35 +0530

    qstns

--- js/app.js
+++ js/app.js
@@ -15,7 +15,8 @@ let availableOptions = [];
 let correct = 0;
 
 var myImage = document.getElementById('mainImage');
-var imageArray = ["img/0.webp", "img/1.png", "img/2.png", "img/3.png", "img/4.png", "img/5.png", "img/6.png", "img/7.png", "img/8.png", "img/9.png", "img/10.png", "img/11.png", "img/12.png", "img/13.png", "img/14.webp", "img/15.png", "img/16.png", "img/17.png", "img/18.png", "img/19.png"];
+var imageArray = ["img/0.webp", "img/1.png", "img/2.png", "img/3.png", "img/4.png", "img/5.png", "img/6.png", "img/7.png", "img/8.png", "img/9.png", "img/10.png", "img/11.png", "img/12.png", "img/13.png", "img/14.webp", "img/15.png", "img/16.png", "img/17.png", "img/18.png", "img/19.png",
+"img/20.png", "img/21.png", "img/22.png", "img/23.png", "img/24.webp", "img/25.png", "img/26.png", "img/27.png", "img/28.png", "img/29.png", "img/30.png", "img/31.png", "img/32.png", "img/33.png", "img/34.webp", "img/35.png", "img/36.png", "img/37.png", "img/38.png"];
 
 function setAvailableQuestions(){
     const totalQuestion = quiz.length;
--- js/question.js
+++ js/question.js
@@ -78,5 +78,85 @@ const quiz = [
     {
         options:['Mohammed Yasir','Anirudh Thapa','Akash Mishra','Halicharan Narzary'],
         answer:1
+    },
+
+    //update
+
+    {
+        options:['Albino Gomes','Arshdeep Singh','Naveen Kumar','Mohammed Nawas'],
+        answer:0
+    },
+    {
+        options:['Udanta Singh','Jorge Ortiz','Amrinder Singh','Ishan Pandita'],
+        answer:2
+    },
+    {
+        options:['Balwant Singh','Amrinder Singh','Subratha Paul','Arshdeep Singh'],
+        answer:0
+    },
+    {
+        options:['Rahul KP','Aniket Jadhav','Rahul Bheke','Lallianzuala Chhangthe'],
+        answer:2
+    },
+    {
+        options:['Anwar Ali','Amarjit Singh Kiyam','Daniel Lalahlimpuia','Aniket Jadhav'],
+        answer:2
+    },
+    {
+        options:['Muhammed Nawaz','Albino Gomez','Arshdeep Singh','Debjit Majumder'],
+        answer:3
+    },
+    {
+        options:['Udanta Singh','Aniket Jadhav','Dimas Delgado','Lallianzuala Chhangthe'],
+        answer:2
+    },
+    {
+        options:['Eugeneson Lyngdoh','Mobashir Rahman','Boris Singh','Ishan Pandita'],
+        answer:0
+    },
+    {
+        options:['Jerri Lalrinzuala','Sahal Samad','Balwant Singh','Mourtada Fall'],
+        answer:3
+    },
+    {
+        options:['Mohammed Yasir','Fedrico Gallego','Akash Mishra','Halicharan Narzary'],
+        answer:1
+    },
+    {
+        options:['Hugo Boumous','Mobashir Rahman','Lallianzuala Chhangthe','Rahim Ali'],
+        answer:0
+    },
+    {
+        options:['Udanta Singh','Ishan Pandita','Jackichand Singh','Komal Thatal'],
+        answer:2
+    },
+    {
+        options:['Albono Gomez','Subhasish Roy','Jitendra Singh','Naveen Kumar'],
+        answer:2
+    },
+    {
+        options:['Khabra','Anirudh Thapa','Boris Singh','Hitesh Sharma'],
+        answer:0
+    },
+    {
+        options:['Lenny Rodrigues','Anwar Ali','Gurpreet Singh','Arshdeep Singh'],
+        answer:0
+    },
+    {
+        options:['Amey Ranawade','Aniket Jadhav','Souvik Chakrabarti','Marcelinho'],
+        answer:3
+    },
+    {
+        options:['Anwar Ali','Memo','Amarjit Singh Kiyam','Aniket Jadhav'],
+        answer:1
+    },
+    {
+        options:['Jerri Lalrinzuala','Jayesh Rane','Balwant Singh','Nandhakumar Shekhar'],
+        answer:3
+    },
+    {
+        //38.png
+        options:['Narender Gahlot','Anwar Ali','Narender Gehlot','Nishu Kumar'],
+        answer:2
     }
 ]
