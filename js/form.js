93abac01fc30987ae6d6595cbdc93e7fe3d64e1d
football in india <footballindia.insta@gmail.com>
2021-06-21 07:42:56 +0530

    google sign undo

--- js/form.js
+++ js/form.js
@@ -54,12 +54,14 @@ var firebaseConfig = {
       // ...
     } else {
       // User is signed out
+      // ...
       console.log("Not logged")
       formlogin.classList.remove("hide");
       formSign.classList.add("hide");
       formProfile.classList.add("hide");
     }
   });
+  
 
   var database = firebase.database()
 
@@ -185,34 +187,6 @@ if(profile_edit_form){
     alert("User name updated successfully!")
   cancelChange();
   })
-}
-
-function googleSign(){
-  var provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithRedirect(provider);
-  firebase.auth()
-  .getRedirectResult()
-  .then((result) => {
-    if (result.credential) {
-      /** @type {firebase.auth.OAuthCredential} */
-      var credential = result.credential;
-
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = credential.accessToken;
-      // ...
-    }
-    // The signed-in user info.
-    var user = result.user;
-  }).catch((error) => {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
 }
 
 function getUser(){
